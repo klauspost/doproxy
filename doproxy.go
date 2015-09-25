d58a960bfc3288cbbba607274bf6ea4e7d11fae1
klauspost <klauspost@gmail.com>
2015-09-25 12:30:34 +0200

    Add commandline parameter for config file.

--- doproxy.go
+++ doproxy.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"flag"
 	"github.com/klauspost/doproxy/server"
 	"github.com/klauspost/shutdown"
+	"log"
 	"os"
 	"syscall"
 	"time"
-	"log"
 )
 
+var configfile = flag.String("config", "doproxy.toml", "Use this config file")
+
 func main() {
+	flag.Parse()
 	shutdown.Logger = log.New(os.Stdout, "", log.LstdFlags)
 	shutdown.OnSignal(0, os.Interrupt, syscall.SIGTERM)
 	shutdown.SetTimeout(time.Second)
-	s, err := server.NewServer("doproxy.toml")
+	s, err := server.NewServer(*configfile)
 	if err != nil {
 		log.Fatal("Error loading server configuration:", err)
 	}
