0359641a4db2bcbf918eaafdcb784720a1faccb3
Klaus Post <klauspost@gmail.com>
2015-09-26 19:17:01 +0200

    Add "least connections" load balancer test.

--- server/loadbalancing_test.go
+++ server/loadbalancing_test.go
@@ -7,6 +7,7 @@ import (
 func TestRoundRobin(t *testing.T) {
 	conf := LBConfig{Type: "roundrobin"}
 	inv := newMockInventory(t, 5)
+	defer inv.Close()
 
 	lb, err := NewLoadBalancer(conf, inv)
 	if err != nil {
@@ -54,5 +55,85 @@ func TestRoundRobin(t *testing.T) {
 	be := lb.Backend()
 	if be != nil {
 		t.Fatal("all backends should be unhealthy, but got one anyway")
+	}
+}
+
+type leastConnTest struct {
+	conns     []int // Connection numbers to simulate
+	expect    []int // Which results (indexes into conns) are allowed
+	unhealthy []int // Which backends should be marked unhealthy
+}
+
+var leastConnTests = []leastConnTest{
+	leastConnTest{conns: []int{1, 0, 0, 0}, expect: []int{1, 2, 3}},
+	leastConnTest{conns: []int{1, 2, 3, 4}, expect: []int{0}},
+	leastConnTest{conns: []int{4, 3, 2, 1}, expect: []int{3}},
+	leastConnTest{conns: []int{0, 1, 1, 1}, expect: []int{0}},
+	leastConnTest{conns: []int{1, 0, 1, 1}, expect: []int{1}},
+	leastConnTest{conns: []int{1, 1, 0, 1}, expect: []int{2}},
+	leastConnTest{conns: []int{1, 1, 1, 0}, expect: []int{3}},
+	leastConnTest{conns: []int{1000}, expect: []int{0}},
+	leastConnTest{conns: []int{0}, expect: []int{0}},
+	leastConnTest{conns: []int{5000, 4000, 3000, 2000, 1000, 100, 50}, expect: []int{6}},
+
+	leastConnTest{conns: []int{1, 0, 0, 0}, expect: []int{1}, unhealthy: []int{2, 3}},
+	leastConnTest{conns: []int{1, 2, 3, 4}, expect: []int{0}, unhealthy: []int{2, 3}},
+	leastConnTest{conns: []int{4, 3, 2, 1}, expect: []int{1}, unhealthy: []int{2, 3}},
+	leastConnTest{conns: []int{0, 1, 1, 1}, expect: []int{1, 2, 3}, unhealthy: []int{0}},
+	leastConnTest{conns: []int{1, 0, 1, 1}, expect: []int{2, 3}, unhealthy: []int{0, 1}},
+	leastConnTest{conns: []int{1, 1, 0, 1}, expect: []int{2}, unhealthy: []int{0, 1}},
+	leastConnTest{conns: []int{1, 1, 1, 0}, expect: []int{3}, unhealthy: []int{0, 1}},
+	leastConnTest{conns: []int{1000}, expect: []int{}, unhealthy: []int{0}},
+	leastConnTest{conns: []int{5000, 4000, 3000, 2000, 1000, 100, 25}, expect: []int{4}, unhealthy: []int{6, 5}},
+	leastConnTest{conns: []int{50, 4000, 3000, 2000, 1000, 100, 25}, expect: []int{0}, unhealthy: []int{6, 5}},
+}
+
+func TestLeastConn(t *testing.T) {
+	conf := LBConfig{Type: "leastconn"}
+	for i, test := range leastConnTests {
+		inv := newMockInventory(t, len(test.conns))
+		lb, err := NewLoadBalancer(conf, inv)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, n := range test.unhealthy {
+			mark := inv.backends[n].(*mockBackend)
+			mark.backend.Close() // Close the monitor, so it doesn't interfere.
+			mark.Stats.mu.Lock()
+			mark.Stats.Healthy = false
+			mark.Stats.mu.Unlock()
+			healthy := inv.backends[n].Healthy()
+			if healthy {
+				t.Fatal("test", i, "Healthy was not false")
+			}
+		}
+		for n, num := range test.conns {
+			mark := inv.backends[n].(*mockBackend)
+			mark.rt.mu.Lock()
+			mark.rt.running = num
+			mark.rt.mu.Unlock()
+			connections := inv.backends[n].Connections()
+			if connections != num {
+				t.Fatal("test", i, "Connections was not set to", num, "got", connections)
+			}
+		}
+		be := lb.Backend()
+		if len(test.expect) == 0 {
+			if be != nil {
+				t.Fatal("test", i, "did not expect any backends, but got number", be)
+			}
+			continue
+		}
+		mbe := be.(*mockBackend)
+		found := false
+		for _, n := range test.expect {
+			if mbe.n == n {
+				found = true
+			}
+		}
+		if !found {
+			t.Fatal("test", i, "unexpected backend. Got", mbe.n, "expected one of", test.expect)
+		}
+		inv.Close()
 	}
 }
